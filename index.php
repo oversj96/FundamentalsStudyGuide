d224629b10f41500607b4d8565a64da6210a40c0
oversj96 <45669103+oversj96@users.noreply.github.com>
2019-05-04 02:17:12 -0500

    corrected formatting

--- index.php
+++ index.php
@@ -34,42 +34,42 @@
 			<div class="notification is-dark" id ="theorems">
 				<strong><center>Definitions and Theorems</center></strong>
 			</div>
-		<?php
-		require_once('docs/mysqli_connect.php');
-		$query = "SELECT question, answer
-		          FROM studyinfo";
-		$response = @mysqli_query($conn, $query);
+			<?php
+			require_once('docs/mysqli_connect.php');
+			$query = "SELECT question, answer
+			FROM studyinfo";
+			$response = @mysqli_query($conn, $query);
 
-		$jqueryInside = "";
-		$count = 0;
-		while($responseRow = $response->fetch_assoc()){
-			$count++;
-			echo "<div class=\"columns\">
+			$jqueryInside = "";
+			$count = 0;
+			while($responseRow = $response->fetch_assoc()){
+				$count++;
+				echo "<div class=\"columns\">
 				<div class=\"column\">
-					<div class=\"notification\">
-						<p>
-							{$responseRow['question']}
-						</p>
-					</div>
+				<div class=\"notification\">
+				<p>
+				{$responseRow['question']}
+				</p>
+				</div>
 				</div>
 				<div class=\"column\">
-					<button class=\"button is-info is-outlined is-large is-fullwidth\" id=\"butDefS{$count}\">Solution</button>
-					<p class=\"section notification is-bold hidden\" id=\"defS{$count}\">
-					{$responseRow["answer"]}
-					</p>
+				<button class=\"button is-info is-outlined is-large is-fullwidth\" id=\"butDefS{$count}\">Solution</button>
+				<p class=\"section notification is-bold hidden\" id=\"defS{$count}\">
+				{$responseRow["answer"]}
+				</p>
 				</div>
-			</div>";
-			$jqueryInside .= '$("#butDefS' . $count . '").click(function(){
-  													$("#defS' . $count . '").fadeToggle("slow");
-  											});';
-		}
-		echo "<script>
-						$(document).ready(function(){
-							{$jqueryInside}
-						});
-					</script>";
-		$conn->close();
-		?> 
-	</section>
-</body>
-</html>
+				</div>";
+				$jqueryInside .= '$("#butDefS' . $count . '").click(function(){
+					$("#defS' . $count . '").fadeToggle("slow");
+				});';
+			}
+			echo "<script>
+			$(document).ready(function(){
+				{$jqueryInside}
+				});
+				</script>";
+				$conn->close();
+				?> 
+			</section>
+		</body>
+		</html>
